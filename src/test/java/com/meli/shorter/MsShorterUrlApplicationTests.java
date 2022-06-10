4b89351de0b89ebb2604182ff406e62e80d89626
cesar <$kat38360$888>
2022-06-10 11:45:58 -0400

    Initial Commit MeLi Challange

---
+++ src/test/java/com/meli/shorter/MsShorterUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.shorter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsShorterUrlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
